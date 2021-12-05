91b907cf897b71649f9806dec2e1fe1f317946a6
ggsales <giovanisales14@gmail.com>
2021-12-04 21:16:20 -0300

    cria array para guardar despesas

--- app.js
+++ app.js
@@ -39,6 +39,23 @@ class Bd {
 
       localStorage.setItem('id', id )
     }
+    //recuperar todas as despesas cadastradas em localstorage
+    recuperarTodosRegistros(){
+        // array de despesas
+        let despesas = Array()
+        let id = localStorage.getItem("id")
+        for(let i = 1; i<= id; i++){
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //verifica se existe a possibilidade de haver indices que foram removidos. E caso existam, será pulado
+            if(despesa === null){
+                continue
+            }
+            despesas.push(despesa)
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -68,10 +85,26 @@ function cadastrarDespesa () {
 
     if(despesa.validarDados()){
         //bd.gravar(despesa)
-        alert('dados validos')
+        document.getElementById('modal_titulo_div').className = "modal-header text-success"
+        document.getElementById('modal_titulo').innerHTML = "Resgistro cadastrado com sucesso"
+        document.getElementById("modal_conteudo").innerHTML="Despesa cadastrada com sucesso"
+        document.getElementById("modal_btn").innerHTML = "voltar"
+        document.getElementById("modal_btn").className = "btn btn-success"
+        $("#modalRegistroDespesa").modal("show")
     } else{
-        alert('insira os dados')
+        document.getElementById('modal_titulo').innerHTML = "Erro na inclusão do registro"
+        document.getElementById("modal_titulo_div").className= "modal-header text-danger"
+        document.getElementById("modal_conteudo").innerHTML="erro na gravação! verifique se todos os campos foram preenchidos corretamente "
+        document.getElementById("modal_btn").innerHTML = "voltar e corrigir"
+        document.getElementById("modal_btn").className = "btn btn-danger"
+        $("#modalRegistroDespesa").modal("show")
     }
     
+}
+
+
+function carregaListaDespesas(){
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
 }
 
